9f6cf62d416e612436be824949cc682ae1cce0fb
Harnas_20 <Harnas_20@o2.pl>
2018-10-18 23:13:25 +0200

    react-twit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Twit from "twit";
 
+
+
+var T = new Twit({
+consumer_key: 's9XnKnMLHYJN1K3WjoARUTMEg',
+consumer_secret: 'xsy4stwAMSlpC4Zyh01GIvfFt5gugcMVMh1F4tuGx2K3rAL3sg ',
+access_token:         '1052166984333430786-J7QlgW6XyWdNK88gypacrvuMvQq0ul ',
+access_token_secret:  'ksfy8ZoXsde0okPau29WOREfreinQHBrTKXVBNrr3nUWQ',
+timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+
+});
+
+T.get('search/tweets', { q: 'test', count: 1 }, gotData);
+
+function gotData(err, data, response) {
+  console.log(data)
+return data
+};
+
+var x = gotData();
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <p> {x}  </p>
         </header>
       </div>
     );
